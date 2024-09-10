3b2378eff8d2201ce7309272daccbae2cb890cad
james <rodriguesjames123@gmail.com>
2024-09-10 20:18:11 +0800

    Rest of the Code

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.utils import retrieve_data_from_db
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join('artifacts','train.csv')
+    test_data_path = os.path.join('artifacts','test.csv')
+    raw_data_path = os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        df = retrieve_data_from_db(r'C:\Users\James\OneDrive\Desktop\house_price_prediction\house_data.db','house_data')
+
+        #covert features to numerical
+        df['area'] = pd.to_numeric(df['area'])
+        df['price'] = pd.to_numeric(df['price'])
+        
+        #remove any whitspace in column name
+        df.columns = df.columns.str.replace(r'\s+', '', regex=True)
+
+        #remove some outliers
+        area_threshold = df['area'].quantile(0.99)
+        price_threshold = df['price'].quantile(0.99)
+        df = df[(df['area'] <= area_threshold) & (df['price'] <= price_threshold)]
+        
+        #creating new columns, dropping unwanted columns
+        df['Year'] = "20" + df['contractDate'].astype(str).str[-2:]
+        df['Year'] = pd.to_numeric(df['Year'])
+        
+        district_to_region = {
+            '01': 'Central', '02': 'Central', '03': 'Central', '04': 'Central', '05': 'West', '06': 'Central',
+            '07': 'Central', '08': 'Central', '09': 'Central', '10': 'Central', '11': 'Central',
+            '12': 'East', '13': 'East', '14': 'East', '15': 'East', '16': 'East', '17': 'East', '18': 'East',
+            '19': 'North-East', '20': 'North-East', '21': 'Central', '22': 'West', '23': 'West', '24': 'West',
+            '25': 'North', '26': 'North', '27': 'North', '28': 'North-East'
+        }
+        
+        df['region'] = df['district'].map(district_to_region)
+        df = df.drop(['tenure','noOfUnits','contractDate','district'],axis=1)
+        
+        df['floorRange'] = df['floorRange'].apply(lambda x: 'Ground' if x == '-' else x)
+        df['typeOfSale'] = df['typeOfSale'].map({ '1' : 'New Sale', '2': 'Sub Sale', '3': 'Resale'})
+        
+    
+        #create artifacts dir
+        os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+        df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+        train_data, test_data = train_test_split(df,test_size=0.2,random_state=42)
+        train_data.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+        test_data.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+        return(
+            self.ingestion_config.train_data_path,
+            self.ingestion_config.test_data_path
+        )
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_path, test_path = obj.initiate_data_ingestion()
+
+    data_transformation_obj = DataTransformation()
+    train_df,test_df,_ = data_transformation_obj.initiate_data_transformation(train_path,test_path)
+
+    model_trainer_obj = ModelTrainer()
+    model_trainer_obj.initiate_model_training(train_df,test_df)
---
+++ src/utils.py
@@ -0,0 +1,64 @@
+import sqlite3
+import pandas as pd
+import numpy as np
+import os
+import sys
+import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+
+
+def retrieve_data_from_db(db_path, table_name):
+    """
+    Retrieve data from the specified table in the SQLite database and return it as a Pandas DataFrame.
+
+    Parameters:
+        db_path (str): Full path to the SQLite database file.
+        table_name (str): Name of the table to retrieve data from. Default is 'house_data'.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the retrieved data.
+    """
+   
+    conn = sqlite3.connect(db_path)
+    query = f"SELECT * FROM {table_name}"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    
+    return df
+
+
+def save_object(file_path,obj):
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path,exist_ok=True)
+
+    with open(file_path,'wb') as f:
+        dill.dump(obj,f)
+
+def load_object(file_path):
+    with open(file_path,'rb') as f:
+        return dill.load(f)
+    
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models,params):
+    report = {}
+    for i in range(len(list(models))):
+        model = list(models.values())[i]
+
+        param = params[list(models.keys())[i]]
+        gs = GridSearchCV(model,param,cv=3)
+        gs.fit(X_train,y_train)
+        model.set_params(**gs.best_params_)
+        model.fit(X_train, y_train)
+
+
+        #predicting on the training and test set
+        y_train_pred = model.predict(X_train)
+        y_test_pred = model.predict(X_test)
+
+        train_model_score = r2_score(y_train,y_train_pred)
+        test_model_score = r2_score(y_test, y_test_pred)
+        report[list(models.keys())[i]] = test_model_score  
+    return report
